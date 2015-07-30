52f6b0555534e2f959bba33f877830f0863a8f50
Harish Kayarohanam <coderman1989@gmail.com>
2015-07-31 00:52:47 +0530

    made a few changes to the test case for issue 12135(https://github.com/elastic/elasticsearch/issues/12135)

--- 12135.py
+++ 12135.py
@@ -1,9 +1,15 @@
 from E import *
 
-#create index named 
+# procedure
+# 1. create index
+# 2. create type and its mappings with @timestamp field mapped to date
+# 3. put values in the field mapped as date
+# 4. search for unix epoch time stamp by giving the value given in step 3
+
+#1. create index named i1 
 post("http://localhost:9200/i1");
 
-#create type and its mappings
+#2. create type and its mappings
 mappings = {
        "properties" : { 
            "@timestamp" : {
@@ -17,14 +23,14 @@ mappings = {
 
 post("http://localhost:9200/i1/i1_typeone/_mapping", mappings);
 
-#create an entry in the above type
+#3. create an entry in the above type
 data = {
   "name" : "harish",
   "@timestamp":1438142633505
 };
 post("http://localhost:9200/i1/i1_typeone/1", data);
 
-#match query - search
+#4. match query - search
 search = {
   "query": {
     "match": { 
