d563c7bceff67aa26242c6724ff1aac665efccc7
Harish Kayarohanam <coderman1989@gmail.com>
2015-07-31 02:21:31 +0530

    adding the test case for issue 12135 (https://github.com/elastic/elasticsearch/issues/12135)

---
+++ 12135.py
@@ -0,0 +1,46 @@
+from ET import *
+
+# 1. create an index(cast) and type(type) and post the data given
+# 2. create another index (cast2) and type (type) with mappings of data to ip
+# 3. post data to index.type in step 2
+# 4. implement the search query cast,cast2/_search?search_type=count
+
+#********************************************************
+#1
+data1 = {
+          "ip_str":"0.0.0.0"
+        }
+post("http://localhost:9200/cast/type/1", data1)
+
+#********************************************************
+#2
+post("http://localhost:9200/cast2");
+mappings = {
+             "properties" : {
+               "ip_str":{
+                 "type":"ip"
+                }
+              }
+           }
+post("http://localhost:9200/cast2/type/_mapping", mappings);
+
+#*********************************************************
+#3
+data2 = {
+          "ip_str":"0.0.0.0"
+        }
+post("http://localhost:9200/cast2/type/1", data2);
+
+#*********************************************************
+#4
+search = {
+           "aggs": {
+             "NAME": {
+               "terms": {
+                 "field": "ip_str",
+                 "size": 10
+               }
+             }
+           }
+         }
+post("http://localhost:9200/cast,cast2/_search?search_type=count", search);
