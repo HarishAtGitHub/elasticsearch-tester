a1ab3dd3138d798a0bdae8fbb9d93963cc2de9e2
Harish Kayarohanam <coderman1989@gmail.com>
2015-08-12 15:52:30 +0530

    made index names global

--- 12790.py
+++ 12790.py
@@ -6,19 +6,23 @@ from ET import *
 # 3. use result from above to create new index.
 # 4. get settings of the index of step 3
 
-#********************************************************
+indexone_name = "index1";
+indextwo_name = "index2";
+#*******************************************************
 #1
-put("http://localhost:9200/indextry1")
+put("http://localhost:9200/%s" % (indexone_name))
 
 #********************************************************
 #2
-indexone_settings = get("http://localhost:9200/indextry1/_settings");
-settings = indexone_settings.get("indextry1");
+indexone_settings = get("http://localhost:9200/%s/_settings" % (indexone_name));
+settings = indexone_settings.get(indexone_name);
 
 #*********************************************************
 #3
-put("http://localhost:9200/indextry2", settings);
+put("http://localhost:9200/%s" % (indextwo_name), settings);
 
 #********************************************************
 #4
-get("http://localhost:9200/indextry2/_settings");
+get("http://localhost:9200/%s/_settings" % (indextwo_name));
+
+#NOTE: for repeated test case just change the indexone_name & indextwo_name values above and run
