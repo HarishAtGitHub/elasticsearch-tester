5bd88ae2e42ead751eca401ab2a13f8228d3bf77
Harish Kayarohanam <coderman1989@gmail.com>
2015-08-12 18:46:05 +0530

    made the test case more sensible

--- 12790.py
+++ 12790.py
@@ -1,28 +1,37 @@
 from ET import *
+import sys
 
 # issue- 12790 : https://github.com/elastic/elasticsearch/issues/12790
-# 1. create an index with name indexone_name.
-# 2. get the settings of the index.
-# 3. use result from above to create new index indextwo_name.
-# 4. get settings of the index of step 3
+# 1. create an index with settings having creation_date and version
+# 2. get settings of the index of step 1 
+# Observed: the new index settings has creation_date and version same as settings given
+# Expected: the new index settings should have creation_date and version to new updated values
 
 indexone_name = "index1";
-indextwo_name = "index2";
+if sys.argv[1:]:
+  indexone_name = sys.argv[1];
 #*******************************************************
 #1
-put("http://localhost:9200/%s" % (indexone_name))
+settings            = {'settings': {
+                                     'index': {
+                                                 'number_of_replicas': '1', 
+                                                 'version': {'created': '2000001'}, 
+                                                 'creation_date': '1439384094544', 
+                                                 'uuid': 'ayzHgwB3Sgey-Pk_Okgwyg', 
+                                                 'number_of_shards': '5'
+                                              }
+                                   }
+                      }
+put("http://localhost:9200/%s" % (indexone_name), settings);
 
 #********************************************************
 #2
-indexone_settings = get("http://localhost:9200/%s/_settings" % (indexone_name));
-settings = indexone_settings.get(indexone_name);
+new_index = get("http://localhost:9200/%s/_settings" % (indexone_name));
+print("input settings \n");
+print(settings);
+print("\n");
+print("new index settings \n");
+print(new_index.get(indexone_name));
 
-#*********************************************************
-#3
-put("http://localhost:9200/%s" % (indextwo_name), settings);
-
-#********************************************************
-#4
-get("http://localhost:9200/%s/_settings" % (indextwo_name));
-
-#NOTE: for repeated test case just change the indexone_name & indextwo_name values above and run
+#NOTE: for repeated test case just change the indexone_name value above and run
+# or run with command line arguments
